616b71732ce77b6d7fae1679f95fafff362e38b9
Glitch (glitch-hello-website) <none>
2023-07-15 15:02:28 +0000

    ❤️😚 Checkpoint ./weatherd.csv:57629255/8516 ./server.py:57629255/1636 ./requirements.txt:57629255/8

--- server.py
+++ server.py
@@ -1,33 +1,29 @@
-from flask import *
-import numpy as  np
+from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.svm import SVC
-app=Flask(__name__)
+from sklearn.ensemble import RandomForestClassifier
+
+app = Flask(__name__)
 
 @app.route('/')
 def weather():
-  return render_template("index.html")
+    return render_template("index.html")
 
-@app.route("/weather",methods=["POST"])
+@app.route("/weather", methods=["POST"])
 def page():
-  temp_max=eval(request.form.get("temp_max"))
-  temp_min=eval(request.form.get("temp_min"))
-  wind=eval(request.form.get("wind"))
-  
-  url="weatherd.csv"
-  data=pd.read_csv(url, header=None)
-  weather=data.values
-  x=weather[:,1:4]
-  y=weather[:,-1]
-  
-  model=SVC()
-  model.fit(x,y)
-  
-  arr=model.predict([[temp_max,temp_min,wind]])
-  return render_template("index.html",result=arr[0])
+    temp_max = eval(request.form.get("temp_max"))
+    temp_min = eval(request.form.get("temp_min"))
+    wind = eval(request.form.get("wind"))
+
+    url = "weatherd.csv"
+    data = pd.read_csv(url)
+    X = data[['temp_max', 'temp_min', 'wind']]
+    y = data['weather']
+
+    model = RandomForestClassifier()
+    model.fit(X, y)
+
+    arr = model.predict([[temp_max, temp_min, wind]])
+    return render_template("index.html", result=arr[0])
+
 if __name__ == '__main__':
-  app.run()
-    
-  
-  
-  
+    app.run()
