ed52984d7dbc5002ce6692d8683c1f14fc41cc9f
Glitch (glitch-hello-website) <none>
2023-07-30 17:06:17 +0000

    🌠🏔 Checkpoint ./server.py:59592511/1337

--- server.py
+++ server.py
@@ -1,29 +1,36 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 app = Flask(__name__)
 
 @app.route('/')
-def weather():
-    return render_template("index.html")
+def Breast Cancer():
+    return render_template("Breast cancer.html")
 
-@app.route("/weather", methods=["POST"])
+@app.route("/breast cancer", methods=["POST"])
 def page():
-    temp_max = eval(request.form.get("temp_max"))
-    temp_min = eval(request.form.get("temp_min"))
-    wind = eval(request.form.get("wind"))
+    radius_mean = eval(request.form.get("radius_mean"))
+    texture_mean = eval(request.form.get("texture_mean"))
+    smoothness_mean = eval(request.form.get("smoothness_mean"))
+    compactness_mean = eval(request.form.get("compactness_mean"))
+    symmetry_mean = eval(request.form.get("symmetry_mean"))
+    radius_se = eval(request.form.get("radius_se"))
+    smoothness_se = eval(request.form.get("smoothness_se"))
+    compactness_se = eval(request.form.get("compactness_se"))
+    radius_worst = eval(request.form.get("radius_worst"))
+    texture_worst = eval(request.form.get("texture_worst"))
 
-    url = "weatherd.csv"
+    url = "New_.csv(1)"
     data = pd.read_csv(url)
-    X = data[['temp_max', 'temp_min', 'wind']]
-    y = data['weather']
+    X = data[['radius_mean', 'texture_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean', 'radius_se', 'smoothness_se', 'compactness_se', 'radius_worst', 'texture_worst']]
+    y = data['diagnosis']
 
-    model = RandomForestClassifier()
+    model = LogisticRegression()
     model.fit(X, y)
 
     arr = model.predict([[temp_max, temp_min, wind]])
-    return render_template("index.html", result=arr[0])
+    return render_template("Breast cancer.html", result=arr[0])
 
 if __name__ == '__main__':
     app.run()
